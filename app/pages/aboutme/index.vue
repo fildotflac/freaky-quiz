9580606ed027b8db0e446209175ff29e594131b7
fildotflac <tsbfpp@gmail.com>
2025-10-02 11:02:38 +0200

    About me mods

--- app/pages/aboutme/index.vue
+++ app/pages/aboutme/index.vue
@@ -20,8 +20,9 @@ useHead({
         <div class="page-container">
             <h1>About me</h1>
             <p>Ciao, sono Filippo Tusberti, studente di Informatica all'ITIS Vinci di Carpi (MO)</p>
-            <p>Questo é un progetto nato da curiosità personale e per spronare al dialogo riguardo l'Intelligenza Artificiale e come potrá impattare sulla società in futuro.</p>
-            <p>Abbiamo creato l'intelligenza artificiale a nostra immagine, nel nostro concreto potremmo definirci degli dei, ma se lo studente superasse il maestro?</p>
+            <p>Questo é un progetto nato da curiosità personale e per spronare al dialogo riguardo l'Intelligenza Artificiale e come potrá impattare la società in futuro.</p>
+            <p>Il sito é creato sfruttando il web framework <a href="https://vuejs.org">Vue.js</a>, non sono stati usate component libraries</p>
+            <p><strong>Il sito é stato creato con aiuto da parte del mio amico <a href="https://claude.ai">claduio 4.5</a>.</strong></p>
         </div>
     </main>
     <Footer />
